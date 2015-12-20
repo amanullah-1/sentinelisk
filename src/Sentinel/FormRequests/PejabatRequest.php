064c25948a3afb4d29245ab8a362985f0048eac1
Iskandar Ali <miskandarmali@gmail.com>
2015-12-20 17:23:46 +0800

    Create PejabatRequest.php

---
+++ src/Sentinel/FormRequests/PejabatRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Sentinel\FormRequests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PejabatRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'jenis_pejabat' =>  'required',
+            'nama' => 'required',
+            'negeri'    =>  'required'
+        ];
+    }
+}
