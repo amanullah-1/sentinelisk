b79e1d08ae9c248693eb99f005fb5e9713f88929
Iskandar Ali <miskandarmali@gmail.com>
2015-12-20 17:31:15 +0800

    Update create.blade.php

--- src/views/bootstrap/pejabat/create.blade.php
+++ src/views/bootstrap/pejabat/create.blade.php
@@ -13,25 +13,27 @@
            <div class="panel-body">
            	<form action="{{ route('sentinel.pejabat.store') }}" method="POST" class="form-horizontal">
 					{{ csrf_field() }}
-					<div class="form-group">
+					<div class="form-group {{ ($errors->has('jenis_pejabat')) ? 'has-error' : '' }}">
 						<label for="task-name" class="col-sm-3 control-label">Jenis Pejabat</label>
 						<div class="col-sm-9">
-							<input type="text" name="jenis_pejabat" id="nama-pejabat" class="form-control" value="{{ old('jenis_pejabat') }}">
+							{!! Form::select('jenis_pejabat',['' => 'Sila pilih', 'Ibu Pejabat' => 'Ibu Pejabat', 'AADK Negeri' => 'AADK Negeri', 'AADK Daerah' => 'AADK Daerah'], null, ['class' => 'form-control']) !!}
+							{!! ($errors->has('jenis_pejabat') ? '<span class="help-block">'.$errors->first('jenis_pejabat').'</span>' : '') !!}
 						</div>
 					</div>
-					<div class="form-group">
-						<label for="task-name" class="col-sm-3 control-label">Negeri</label>
-						<div class="col-sm-9">
-							<input type="text" name="negeri" id="nama-pejabat" class="form-control" value="{{ old('negeri') }}">
-						</div>
-					</div>
-					<!-- Task Name -->
-					<div class="form-group">
+					<div class="form-group {{ ($errors->has('nama')) ? 'has-error' : '' }}">
 						<label for="task-name" class="col-sm-3 control-label">Nama</label>
 						<div class="col-sm-9">
 							<input type="text" name="nama" id="nama-pejabat" class="form-control" value="{{ old('nama') }}">
+							{!! ($errors->has('nama') ? '<span class="help-block">'.$errors->first('nama').'</span>' : '') !!}
 						</div>
 					</div>
+					<div class="form-group {{ ($errors->has('negeri')) ? 'has-error' : '' }}">
+						<label for="task-name" class="col-sm-3 control-label">Negeri</label>
+						<div class="col-sm-9">
+							{!! Form::select('negeri', $negeriList, null, ['class' => 'form-control']) !!}
+							{!! ($errors->has('negeri') ? '<span class="help-block">'.$errors->first('negeri').'</span>' : '') !!}
+						</div>
+					</div>				
 					<!-- Add Task Button -->
 					<div class="form-group">
 						<div class="col-sm-offset-3 col-sm-9">
